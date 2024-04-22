88d371a86eb4d0bdcf004c4432d80052b9a2bc3c
MichaelM956 <153692803+MichaelM956@users.noreply.github.com>
2024-04-21 23:08:13 -0500

    third commit

--- signup.cy.js
+++
@@ -1,7 +0,0 @@
-describe('signup on contact list app', () => {
-  it('navigates to signup form when signup button is clicked', () => {
-    cy.visit('https://thinking-tester-contact-list.herokuapp.com/')
-    cy.get('#signup').click()
-  
-  })
-})
